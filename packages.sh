10db853aa0e6ad3069e685b17f0f43e68a7cb464
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2023-10-23 17:39:38 -0600

    fix(hotfix): :adhesive_bandage: Update snap installation
    Modified snap installation as specified in snapcraft.io

--- packages.sh
+++ packages.sh
@@ -38,6 +38,7 @@ sudo pacman -S go go-tools python-docutils
 git clone https://aur.archlinux.org/snapd.git $snap_directory
 cd "$snap_directory"
 makepkg -si
-sudo systemctl enable --now snapd
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
 cd "$HOME"
 notification "✅ Snap successfully installed"
