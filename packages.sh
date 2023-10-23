956dedbe222f1f82fad37d3a3d43b568538f7d7f
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2023-10-23 00:12:26 -0600

    feat(packages): :sparkles: Add AUR helper and Flatpak to packages module
    Implemented code for the instalation of AUR helper and Flatpak

--- packages.sh
+++ packages.sh
@@ -4,6 +4,7 @@ endline="\n\n"
 
 # Path variables
 pacman_mirrorlist="/etc/pacman.d/mirrorlist"
+yay_directory="$HOME/Downloads/yay"
 
 # Actualización inicial
 printf "🔃 PACMAN CONFIGURATION$endline"
@@ -15,3 +16,11 @@ sudo cp $pacman_mirrorlist "$pacman_mirrorlist.bak"
 sudo reflector --verbose --latest 10 --protocol https --sort rate --save $pacman_mirrorlist
 sudo pacman -Sy
 printf "✅ pacman package manager updated and configured$endline"
+
+# Install AUR helper and Flatpak
+git clone https://archlinux.org/yay.git ~/Downloads
+cd "$yay_downloads"
+makepkg -si
+cd "$HOME"
+sudo pacman -S flatpak
+printf "✅ AUR helper and Flatpak successfully installed$endline"
