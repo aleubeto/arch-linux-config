c131f1cb37fce867ea09699a5d61ae834d3e7550
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2023-10-22 20:40:21 -0600

    feat(git): :sparkles: Configurate ssh keys and global variables
    Implemented sh script that configures global variables for git and create (and configure) new ssh keys

---
+++ git_config.sh
@@ -0,0 +1,16 @@
+# Git config variables
+read -p "git user.name: " git_user_name
+read -p "git user.email: " git_user_email
+read -p "git default branch name: " git_default_branch_name
+
+# Git global configurations
+git config --global user.name "$git_user_name"
+git config --global user.email "$git_user_email"
+git config --global init.defaultBranch "$git_default_branch_name"
+echo "✅ git global variables successfully configured"
+
+# Create new SSH keys
+ssh-keygen -t rsa -b 4096 -C "$git_user_email"
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+echo "✅ new SSH keys successfully generated and configured"
