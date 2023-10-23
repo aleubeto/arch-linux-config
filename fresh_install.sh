a02c8b3c8e9e2c78e8e35a478ff171f9f2a335fb
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2023-10-23 00:19:45 -0600

    refactor(install): :twisted_rightwards_arrows: Update main script
    Added recent implemented modules to the main script

--- fresh_install.sh
+++ fresh_install.sh
@@ -3,13 +3,17 @@
 endline="\n\n"
 
 # Grant execution permissions
-chmod +x packages.sh
 chmod +x git_config.sh
+chmod +x packages.sh
+chmod +x system.sh
+chmod +x programming_languages.sh
 chmod +x terminal.sh
 
 # Execute configuration scripts
-./packages.sh
 ./git_config.sh
+./packages.sh
+./system.sh
+./programming_languages.sh
 ./terminal.sh
 
 # Success notification
