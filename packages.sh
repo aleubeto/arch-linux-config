7f5b44a7fa7b24ec7d4a8af82871bf966183c660
Alejandro Ubeto <142447560+alexUbeto@users.noreply.github.com>
2023-10-22 20:11:13 -0600

    feat(packages): :sparkles: Configure and Speed Up Pacman
    Implemented base script for Configuring and Speeding Up Pacman Package Manager

---
+++ packages.sh
@@ -0,0 +1,11 @@
+# Path variables
+pacman_mirrorlist = "/etc/pacman.d/mirrorlist"
+
+# Actualización inicial
+sudo pacman -Syu
+
+# Configure and Speed Up Pacman
+sudo pacman -S reflector
+sudo cp $pacman_mirrorlist "$pacman_mirrorlist.bak"
+sudo reflector --verbose --latest 10 --protocol https --sort rate --save $pacman_mirrorlist
+sudo pacman -Sy
