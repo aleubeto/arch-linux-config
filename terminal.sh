aleubeto/linux-config

fix(terminal): :bug: Fix venv alias command
Fixed the venv alias by adding 3 to the python command. python -> python3.

terminal.sh
@@ -13,7 +13,7 @@ chsh -s $(which zsh)
 notification "✅ zshell successfully installed and configurate"
 
 # Install Oh my Zsh and powerlevel10k
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 notification "✅ Oh my Zsh and powerlevel10k successfully installed"
@@ -30,6 +30,6 @@ notification "✅ nvchad successfully installed"
 # Custom alias
 echo "alias update='sudo apt update; sudo apt upgrade; sudo snap refresh'" >> ~/.zshrc
 echo "alias ssh_keys='cat ~/.ssh/id_rsa.pub | xclip -selection clipboard'" >> ~/.zshrc
-echo "alias venv='python -m venv venv; cat "✅ Successfully created virtual environment"'" >> ~/.zshrc
+echo "alias venv='python3 -m venv venv; cat "✅ Successfully created virtual environment"'" >> ~/.zshrc
 echo "alias activate='source venv/bin/activate'" >> ~/.zshrc
 notification "✅ alias created successfully"

aleubeto/arch-linux-config

terminal.sh
@@ -13,7 +13,7 @@ chsh -s $(which zsh)
 notification "✅ zshell successfully installed and configurate"
 
 # Install Oh my Zsh and powerlevel10k
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 notification "✅ Oh my Zsh and powerlevel10k successfully installed"
@@ -30,6 +30,6 @@ notification "✅ nvchad successfully installed"
 # Custom alias
 echo "alias update='sudo apt update; sudo apt upgrade; sudo snap refresh'" >> ~/.zshrc
 echo "alias ssh_keys='cat ~/.ssh/id_rsa.pub | xclip -selection clipboard'" >> ~/.zshrc
-echo "alias venv='python -m venv venv; cat "✅ Successfully created virtual environment"'" >> ~/.zshrc
+echo "alias venv='python3 -m venv venv; echo "✅ Successfully created virtual environment"'" >> ~/.zshrc
 echo "alias activate='source venv/bin/activate'" >> ~/.zshrc
 notification "✅ alias created successfully"